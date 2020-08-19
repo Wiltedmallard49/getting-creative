c5c37b5d4a4480254d903c14565c017bb1edb655
Wiltedmallard49 <69858723+Wiltedmallard49@users.noreply.github.com>
2020-08-19 12:00:29 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let mySprite = sprites.create(img`
+    f f f f f f f f f f f f f f f f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 f f 5 5 5 5 5 5 f f 5 5 f 
+    f 5 5 f f 5 5 5 5 5 5 f f 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 f 5 5 5 5 5 5 5 5 f 5 5 f 
+    f 5 5 5 f 5 5 5 5 5 5 f 5 5 5 f 
+    f 5 5 5 5 f f f f f f 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+    f f f f f f f f f f f f f f f f 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
