e3d4b247c1d79ffb10b8e35ee62fa20a35b07f6b
Wiltedmallard49 <69858723+Wiltedmallard49@users.noreply.github.com>
2020-08-20 10:21:29 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    mySprite2.setPosition(randint(0, 150), randint(0, 120))
+    music.baDing.play()
+    info.changeScoreBy(1)
+})
+let mySprite2: Sprite = null
+mySprite2.setPosition(randint(0, 150), randint(0, 120))
 let mySprite = sprites.create(img`
     f f f f f f f f f f f f f f f f 
     f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
@@ -16,4 +23,15 @@ let mySprite = sprites.create(img`
     f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
     f f f f f f f f f f f f f f f f 
     `, SpriteKind.Player)
+mySprite2 = sprites.create(img`
+    . . b b b b . . 
+    . b 5 5 5 5 b . 
+    b 5 d 3 3 d 5 b 
+    b 5 3 5 5 1 5 b 
+    c 5 3 5 5 1 d c 
+    c d d 1 1 d d c 
+    . f d d d d f . 
+    . . f f f f . . 
+    `, SpriteKind.Food)
 controller.moveSprite(mySprite)
+music.setVolume(10)
